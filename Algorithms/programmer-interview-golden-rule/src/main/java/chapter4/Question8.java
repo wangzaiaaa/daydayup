98e39e5a01a7799e3a1dd090bb3077801b26f400
wangzaiaaa <2627894288@qq.com>
2021-12-22 15:39:46 +0800

    Chapter4.question8

---
+++ Algorithms/programmer-interview-golden-rule/src/main/java/chapter4/Question8.java
@@ -0,0 +1,16 @@
+package chapter4;
+
+import top.zw.datastruct.tree.TreeNode;
+
+public class Question8 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(left != null && right != null) return root;
+        return left == null ? right : left;
+    }
+
+
+}
